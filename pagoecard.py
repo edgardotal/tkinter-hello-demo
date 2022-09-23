fde74813ccb5a9240cfd65c68cf02e0b4e7a449c
edgardotal <33851413+edgardotal@users.noreply.github.com>
2022-09-23 19:39:55 -0400

    Update pagoecard.py

--- pagoecard.py
+++ pagoecard.py
@@ -1,9 +1,9 @@
+from os import path
 import webbrowser as wb
 from PIL import Image
-from PIL import ImageTk
-import tkinter
+#from PIL import ImageTk
+#import tkinter
 import tkinter as tk
-from os import path
 import os, sys
 
 
@@ -14,10 +14,10 @@ def acceso_web():
 
 
 
-ventana= tk.Tk()
+ventana= tk()
 ventana.title("Boton de Pago")
 ventana.geometry("350x150")
-imagen = ImageTk.PhotoImage(Image.open("logologin.jpeg"))
+imagen = Image.PhotoImage(Image.open("logologin.jpeg"))
 boton = tk.Button(ventana,image =imagen,command=acceso_web)
 boton.place(x=10, y=10)
 ventana.mainloop()
