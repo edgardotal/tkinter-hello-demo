85964b99807a823789bb914ebab724bedd6eb005
edgardotal <33851413+edgardotal@users.noreply.github.com>
2022-09-24 08:33:50 -0400

    Update pagoecard.py

--- pagoecard.py
+++ pagoecard.py
@@ -1,18 +1,17 @@
-import os, sys
-from os import path
-import webbrowser as wb
-from tkinter import * 
-import tkinter as tk
-from PIL import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
-def acceso_web():
-    #url = wb.open('ecard.center/index/express_checkout/index.php')
-    url = wb.open('https://ecard.center/index/express_checkout/index.php')
 
-ventana = tk()
-ventana.title("Boton de Pago")
-ventana.geometry("350x150")
-imagen = PhotoImage(Image.open("logologin.jpeg"))
-boton = tk.Button(ventana,image =imagen,command=acceso_web)
-boton.place(x=10, y=10)
-ventana.mainloop()
+
+#class MainApp(QMainWindow):
+#	def __init__(self, parent=none, args)
+    
+
+
+app = QApplication([])
+window = QMainWindow()
+window.show()
+app.exec()	
+
+
