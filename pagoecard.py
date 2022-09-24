673d2ba1b04fcf780212ee50e097e7d22debfc1a
edgardotal <33851413+edgardotal@users.noreply.github.com>
2022-09-23 20:28:20 -0400

    Update pagoecard.py

--- pagoecard.py
+++ pagoecard.py
@@ -1,19 +1,14 @@
 import os, sys
 from os import path
 import webbrowser as wb
-from PIL import Image
-#from PIL import ImageTk
-#import tkinter
+from tkinter import *
 import tkinter as tk
-
-
-
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 def acceso_web():
     #url = wb.open('ecard.center/index/express_checkout/index.php')
     url = wb.open('https://ecard.center/index/express_checkout/index.php')
-
-
 
 ventana = tk.Tk()
 ventana.title("Boton de Pago")
